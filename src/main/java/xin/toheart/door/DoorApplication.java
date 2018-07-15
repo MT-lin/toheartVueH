a2d7b56900ee079faa6ab60cae8729ca7a75369a
e <15901544506@163.com>
2018-07-15 16:41:28 +0800

    添加用户登录拦截，常量类

--- src/main/java/xin/toheart/door/DoorApplication.java
+++ src/main/java/xin/toheart/door/DoorApplication.java
@@ -3,9 +3,11 @@ package xin.toheart.door;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("xin.toheart.door.mapper")
+@ComponentScan("xin.toheart.door")
 public class DoorApplication {
 
 	public static void main(String[] args) {
